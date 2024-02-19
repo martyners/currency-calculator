a2579741fbbdf6a6c5f9d3a24a1d5ada60fe2593
Martyna MH <martyna.michalak@gmail.com>
2024-02-19 09:16:19 +0100

    homework5

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
-
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  let currency = currencyElement.value;
-  let amount = amountElement.value;
-
-  let result = currency === "EUR" ? amount / 4.3 : amount / 4.01;
-  resultElement.innerText = result.toFixed(2);
-});
+{
+  const initCurrencyConverter = () => {
+    const formElement = document.querySelector(".js-form");
+    const amountElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
+    const resultElement = document.querySelector(".js-result");
+  
+    const onSubmit = (event) => {
+      event.preventDefault();
+      
+      const currency = currencyElement.value;
+      const amount = amountElement.value;
+  
+      const exchangeRate = currency === "EUR" ? 4.3 : 4.01;
+      const result = (amount / exchangeRate).toFixed(2);
+  
+      resultElement.innerText = result;
+    };
+  
+    formElement.addEventListener("submit", onSubmit);
+  };
+  
+  initCurrencyConverter();
+}
