6737f9e7fc332f7c03513b2c2801f65a2d616c19
Martyna MH <martyna.michalak@gmail.com>
2024-02-20 14:02:16 +0100

    functions updated

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 {
 
-  const calculateResalt = () => {
-    const currencyElement = document.querySelector(".js-currency");
-    const currency = currencyElement.value;
-    const amountElement = document.querySelector(".js-amount");
-    const amount = amountElement.value;
+  const calculateResalt = (amount, currency) => {
     const resultElement = document.querySelector(".js-result");
-    
     const exchangeRate = currency === "EUR" ? 4.3 : 4.01;
     const result = (amount / exchangeRate).toFixed(2);
+    
     resultElement.innerText = result;
   }
 
   const onSubmit = (event) => {
+    const currencyElement = document.querySelector(".js-currency");
+    const currency = currencyElement.value;
+    const amountElement = document.querySelector(".js-amount");
+    const amount = amountElement.value;
+
     event.preventDefault();  
-    calculateResalt();
+    calculateResalt(amount, currency);
   };
   
   
